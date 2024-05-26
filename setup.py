df480837c85edfdf098307f7ca8568de93c4946f
Swapnanil Sharma <swapnanilsharma@gmail.com>
2024-05-26 10:35:28 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="Medical Chatbot",
-    version="0.0.0",
-    author="Boktiar Ahmed Bappy",
-    author_email="entbappy73@gmail.com",
+    name="HealthCare Chatbot",
+    version="0.0.1",
+    author="Swapnanil Sharmah",
+    author_email="swapnanilsharma@gmail.com",
     packages=find_packages(),
     install_requires=[],
 )
